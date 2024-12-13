68c8e7305ce2b45b739bba113a0d84a6f0cec89e
buhtig-sudo-azar <azarovvvit@mail.ru>
2024-12-13 16:00:35 +0300

    1.4: kurssitiedot step4

--- Part1/Notes/src/App.jsx
+++ Part1/Notes/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+let uidCounter = 0;
 const Header = (props) => {
 
   return (
@@ -12,41 +12,65 @@ const Header = (props) => {
 const Part = (props) => {
   return (
     <>
-      <p>{props.name}  {props.exercises}</p>
+      {props.name}
     </>
   )
 }
+const Total = (props) => {
+  const { parts } = props;
+  return (
+    <>
+      {
+        parts.map(i =>
+          <p key={Math.random()}>{i.exercises} </p>
+        )
+      }
+
+    </>
+  )
+
+}
 
 const Content = (props) => {
+  const { parts } = props;
 
   return (
     <>
-      <Part name={props.name} exercises={props.exercises} />
+
+      {parts.map(i =>
+        <p key={Math.random()}>  <Part name={i.name} /></p>
+
+      )
+
+      }
+
     </>
   )
+
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <>
       <Header course={course} />
-      <Content name={part1.name} exercises={part1.exercises} />
-      <Content name={part2.name} exercises={part2.exercises} />
-      <Content name={part3.name} exercises={part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </>
   )
 }
