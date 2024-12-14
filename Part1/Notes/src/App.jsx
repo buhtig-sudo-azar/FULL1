ce9282f1c27d431340c06923f6998d27a4641cf6
buhtig-sudo-azar <azarovvvit@mail.ru>
2024-12-14 11:58:27 +0300

    1.5: kurssitiedot step5

--- Part1/Notes/src/App.jsx
+++ Part1/Notes/src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-let uidCounter = 0;
 const Header = (props) => {
 
   return (
@@ -50,27 +49,29 @@ const Content = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   )
 }
