a0ac480b14c23c27f019231af05469d646f1a216
buhtig-sudo-azar <azarovvvit@mail.ru>
2024-12-27 09:44:50 +0300

    1.7: unicafe step2

--- Part1/Unicafe/src/App.jsx
+++ Part1/Unicafe/src/App.jsx
@@ -6,32 +6,47 @@ const Button = (props) => {
   )
 }
 const Display = (props) => {
-  const { good, newtral, bad } = props;
+  const { good, neutral, bad, all, avg, pos } = props;
   return (
     <>
       <h4>Statistics</h4>
       <p>good {good}</p>
-      <p>neutral {newtral}</p>
+      <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+
+
+      <p>average {avg}</p>
+      <p>positive {pos}</p>
     </>
   )
 
 }
 
+
 function App() {
-  const [good, setGood] = useState(0);
-  const [newtral, stNewtral] = useState(0);
-  const [bad, setBad] = useState(0);
+  let [good, setGood] = useState(0);
+  let [neutral, setNeutral] = useState(0);
+  let [bad, setBad] = useState(0);
+  const all = good + bad + neutral;
+
+  const average = () => {
+    if (all == 0) return 0;
+    return ((good - bad) / all)
+  }
+  const positive = () => {
+    return (good / all) * 100 + "%"
+  }
 
   return (
     <>
 
       <h1>Geeve feedback</h1>
       <Button onclick={() => setGood(good => good + 1)} text="good" />
-      <Button onclick={() => stNewtral(newtral => newtral + 1)} text="newtral" />
+      <Button onclick={() => setNeutral(neutral => neutral + 1)} text="neutral" />
       <Button onclick={() => setBad(bad => bad + 1)} text="bad" />
 
-      <Display good={good} newtral={newtral} bad={bad} />
+      <Display good={good} neutral={neutral} bad={bad} all={good + bad + neutral} avg={average()} pos={positive()} />
     </>
   )
 }
