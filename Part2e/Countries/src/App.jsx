14a95cc20a1b9a3fc3e44d86aff89bb664a38b08
buhtig-sudo-azar <167521712+buhtig-sudo-azar@users.noreply.github.com>
2025-04-27 05:36:00 +0000

    2.18* Data for countries, step 1

--- Part2e/Countries/src/App.jsx
+++ Part2e/Countries/src/App.jsx
@@ -1,34 +1,39 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import * as countriesServ from "../services/countriesService"
+import { useState, useEffect } from "react";
+import "./App.css";
+import * as countriesServ from "../services/countriesService";
 
 function App() {
-  const [countries, setCountries] = useState([])
-const [searchTerm, setSearchTerm] = useState('')
+  const [countries, setCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   useEffect(() => {
-  countriesServ.getAll().then(initialCountries => {
-    setCountries(initialCountries); 
-})
-}, []); // <-- С ПУСТЫМ массивом зависимостей
+    countriesServ.getAll().then((initialCountries) => {
+      setCountries(initialCountries);
+    });
+  }, []); // <-- С ПУСТЫМ массивом зависимостей
 
-const searchHandlerCountry =(event)=>{
-setSearchTerm(event.target.value)
-}
+  const searchHandlerCountry = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-const countriesToShow = searchTerm
-    ? countries.filter(country => country.name.common.toLowerCase().includes(searchTerm.toLowerCase())) // <-- Скобка здесь
+  const countriesToShow = searchTerm
+    ? countries.filter((country) =>
+        country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+      ) // <-- Скобка здесь
     : countries;
   return (
     <>
-    <span>finddd countries: <input value={searchTerm} onChange={searchHandlerCountry} /></span>
-       <ul>
-      {countriesToShow.map(country => (
-         // ??? Напиши код для <li> здесь ???
-       <li key={country.cca3}>{country.name.common}</li>
-      ))}
-    </ul>
+      <span>
+        find countries:{" "}
+        <input value={searchTerm} onChange={searchHandlerCountry} />
+      </span>
+      <ul>
+        {countriesToShow.map((country) => (
+          // ??? Напиши код для <li> здесь ???
+          <li key={country.cca3}>{country.name.common}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
