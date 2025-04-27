6b6d7a635a91fd61b2627fd0259094b0ebc73936
buhtig-sudo-azar <167521712+buhtig-sudo-azar@users.noreply.github.com>
2025-04-27 16:44:08 +0000

    2.18* Data for countries, step 1

--- Part2e/Countries/src/App.jsx
+++ Part2e/Countries/src/App.jsx
@@ -16,21 +16,61 @@ function App() {
   };
 
   const countriesToShow = searchTerm
-    ? countries.filter((country) =>
-        country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-    : countries;
+    ? countries.filter((country) => {
+        const countryNameLower = country.name.common.toLowerCase();
+        if (searchTerm.length >= 2) {
+          // Если введено две или больше букв, ищем по первым двум
+          const twoLetters = searchTerm.substring(0, 2).toLowerCase(); // Получаем первые две буквы
+          return countryNameLower.startsWith(twoLetters);
+        } else {
+          // Если введена только одна буква, ищем по первой букве
+          const firstLetter = searchTerm[0].toLowerCase();
+          return countryNameLower.startsWith(firstLetter);
+        }
+      })
+    : [];
+
+  let contentToRender;
+  if (!searchTerm) {
+    contentToRender = <h3>Начните вводить название страны...</h3>;
+  } else if (countriesToShow.length > 10) {
+    contentToRender = <h5>Too many matches, specify another filter!</h5>;
+  } else if (countriesToShow.length < 10 && countriesToShow.length > 0) {
+    contentToRender = (
+      <ul>
+        {countriesToShow.map((country) => (
+          <li key={country.cca3}>{country.name.common}</li>
+        ))}
+      </ul>
+    );
+  } else if (countriesToShow.length == 1) {
+    const onlyCountry = countriesToShow[0];
+    const languages = onlyCountry.languages;
+    const langValues = Object.values(languages);
+    contentToRender = (
+      <div>
+        <h2>{onlyCountry.name.common}</h2>
+        <p>Столица: {onlyCountry.capital}</p>
+        <p>Площадь: {onlyCountry.area} км²</p>
+        Языки:{" "}
+        {langValues.map((language, index) => (
+          <span key={index}>
+            {language}
+            {index < langValues.length - 1 ? ", " : ""}
+          </span>
+        ))}
+      </div>
+    );
+  } else {
+    contentToRender = <p>No countries match your filter</p>;
+  }
   return (
     <>
       <span>
         find countries:{" "}
         <input value={searchTerm} onChange={searchHandlerCountry} />
       </span>
-      <ul>
-        {countriesToShow.map((country) => (
-          <li key={country.cca3}>{country.name.common}</li>
-        ))}
-      </ul>
+      {contentToRender}
     </>
   );
 }
