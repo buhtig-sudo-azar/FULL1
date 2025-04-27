07a4ae4fcd37b57c3788ab7046814fe5d02e05b5
buhtig-sudo-azar <167521712+buhtig-sudo-azar@users.noreply.github.com>
2025-04-27 05:37:29 +0000

    2.18* Data for countries, step 1

--- Part2e/Countries/src/App.jsx
+++ Part2e/Countries/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   return (
     <>
       <span>
-        finddd countries:{" "}
+        findddfff countries:{" "}
         <input value={searchTerm} onChange={searchHandlerCountry} />
       </span>
       <ul>
