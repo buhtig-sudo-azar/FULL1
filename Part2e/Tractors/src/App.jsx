d52c298e8fff53c01516d4d6c9423a005738f723
buhtig-sudo-azar <167521712+buhtig-sudo-azar@users.noreply.github.com>
2025-06-09 03:19:59 +0000

    Traktor

--- Part2e/Tractors/src/App.jsx
+++ Part2e/Tractors/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import * as cheerio from 'cheerio';
+import axios from "axios";
+import * as cheerio from "cheerio";
+import { useState, useEffect } from "react";
 
 function App() {
   const [tractors, setTractors] = useState([]);
-  const [tractorImages, setTractorImages] = useState([]);
 
   useEffect(() => {
-    async function parseTractorData() {
+    async function fetchData() {
       try {
-        // ИЗМЕНИ ЭТОТ URL НА URL ТВОЕГО ПРОКСИ-СЕРВЕРА
-        const response = await axios.get('https://d7f4923d-561c-49fb-9b18-8918ac8ba1dc-00-pvj3jn32njmu.sisko.replit.dev:3000/api/tractordata');
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const images = $('img[src^="https://www.tractordata.com/ltphotos/"]');
-        const imageUrls = images.map((i, el) => $(el).attr('src')).get();
-        setTractorImages(imageUrls);
-        console.log(html); // Посмотри сюда! Что здесь выводится?
+        const response = await axios.get(
+          "https://th89jf-3000.csb.app/api/tractors"
+        );
+        setTractors(response.data); // Сохраняем данные в состоянии
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     }
 
-    parseTractorData();
+    fetchData();
   }, []);
 
   return (
     <div>
-      {tractorImages.map((imageUrl, index) => (
-        <img key={index} src={imageUrl} alt={`Tractor ${index}`} />
-      ))}
+      <h1>Список тракторов</h1>
+      {tractors.length > 0 ? ( // Проверяем, есть ли данные
+        tractors.map((tractor) => (
+          <div key={tractor.model}>
+            <h2>{tractor.model}</h2>
+            <p>Производитель: {tractor.manufacturer}</p>
+            <p>Мощность: {tractor.power} л.с.</p>
+            <p>{tractor.description}</p>
+            {/* <img src={tractor.image} alt={tractor.model} /> */}
+          </div>
+        ))
+      ) : (
+        <p>Загрузка данных...</p>
+      )}
     </div>
   );
 }
